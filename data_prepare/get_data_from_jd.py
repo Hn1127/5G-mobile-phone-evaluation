637b09e1bb7e2f23caf089183dccd7b543dd6eb4
Hn1127 <202130442624@mail.scut.edu.cn>
2024-06-06 10:46:51 +0800

    修改了爬虫程序，现在可以从京东获取手机的基本信息和评论，修改get_data_from_jd.py的21-25行可以修改爬虫的规则

--- data_prepare/get_data_from_jd.py
+++ data_prepare/get_data_from_jd.py
@@ -2,29 +2,42 @@ import time
 import re
 import csv
 import json
+import traceback
+import pandas as pd
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 import os
 
-KeyWord = '5g手机'
+psorts = {
+    '销量': 3,
+    '评论数': 4
+}
+
 MaxPages = 1
 MaxProductName = 10
+MaxCommentNum = 20
+psort = psorts['销量']
+KeyWord = '5g手机'
+
+comment_dirname = 'comment'
+phone_info_jd_filepath = 'phone_info_jd.csv'
+id_map = pd.read_csv(phone_info_jd_filepath)[['ID']]
 
 
-def get_data(key="5g手机", max_pages=1, max_product_num=10):
+def get_data(key="5g手机", max_pages=1, max_product_num=10, psort=3, max_comment_num=20):
     # 使用Chrome浏览器驱动程序
     chrome_options = Options()
     driver = webdriver.Chrome(options=chrome_options)
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
-
-    # 登录并获取cookie
-    # 京东网页的登录界面
+    # 登录京东并获取cookie
     driver.get("https://passport.jd.com/new/login.aspx?/")
     cookie_file = 'cookie.txt'
-    try:
+
+    def login():
         # 判断是否有 cookie.txt 文件
         if os.path.exists(cookie_file):
             # 读取cookie文件中的内容
@@ -46,12 +59,14 @@ def get_data(key="5g手机", max_pages=1, max_product_num=10):
             with open('cookie.txt', 'w') as f:
                 f.write(jsoncookies)
             print('cookies已保存')
+
+    try:
+        login()
     except:
         print('登录出错!')
-        exit(0)
 
     # 发送请求获得搜索内容
-    url = f"https://search.jd.com/Search?keyword={key}"
+    url = f"https://search.jd.com/Search?keyword={key}&psort={psort}"
     driver.get(url)
 
     # 初始化变量
@@ -78,17 +93,23 @@ def get_data(key="5g手机", max_pages=1, max_product_num=10):
                 price = div.find("div", class_="p-price").get_text().strip()
                 if price == '￥':
                     price = '暂无参考价'
-                commit = div.find("div", class_="p-commit").get_text().strip()
-                commit = commit.replace('条评价', '').replace('+', '')
-                if '万' in commit:
-                    commit = float(commit.replace('万', '')) * 10000
-                commit = int(commit)
-
+                comment_count = div.find("div", class_="p-comment")
+                comment_count = comment_count.find("strong").get_text().strip()
+                comment_count = comment_count.replace('条评价', '').replace('+', '')
+                if '万' in comment_count:
+                    comment_count = float(comment_count.replace('万', '')) * 10000
+                comment_count = int(comment_count)
                 # 获取商品详情页链接
                 link = div.find("div", class_="p-name").find("a").get("href")
+                id = link.replace("//item.jd.com/", "").replace(".html", "")
+                if id in id_map:
+                    continue
                 if "http" not in link:
                     link = "https:" + link
-
+            except:
+                traceback.print_exc()
+                print(f'error1:在爬取{name}:{driver.current_url}时出错:获取详情页链接错误')
+            try:
                 # 打开新标签页, 商品详情页
                 driver.execute_script(f'''window.open("{link}","_blank");''')
                 # 切换到新标签页
@@ -98,48 +119,99 @@ def get_data(key="5g手机", max_pages=1, max_product_num=10):
                 time.sleep(3)
                 # 解析html页面
                 html_parse_new = BeautifulSoup(driver.page_source, "html.parser")
-                time.sleep(1)
-
+            except:
+                traceback.print_exc()
+                print(f'error2:在爬取{name}:{driver.current_url}时出错:跳转详情页错误')
+            try:
                 # 获取商品品牌、正规品名以及对应价格
                 brand = html_parse_new.find("a", clstag="shangpin|keycount|product|pinpai_1").get_text().strip()
                 name = html_parse_new.find("li", string=re.compile("商品名称")).get_text().replace('商品名称：',
                                                                                                    '').strip()
-
                 # 查看商品评价
                 tab_menu = driver.find_elements_by_css_selector('#detail > div.tab-main.large > ul > li')
                 for i, btn in enumerate(tab_menu):
                     if "商品评价" in btn.text:
                         btn.click()
                         break
-                time.sleep(1)
+                time.sleep(3)
                 # 获取好评率
-                good_commit_percent = driver.find_element_by_css_selector(
+                good_comment_percent = driver.find_element_by_css_selector(
                     '#comment > div.mc > div.comment-info.J-comment-info > div.comment-percent > div').text
                 # 获取评论关键字
-                commit_tags = driver.find_elements_by_css_selector(
+                comment_tags = driver.find_elements_by_css_selector(
                     '#comment > div.mc > div.comment-info.J-comment-info > div.percent-info > div > span')
                 tags = []
-                for i, tag in enumerate(commit_tags):
+                for i, tag in enumerate(comment_tags):
                     tags.append(tag.text)
-
                 # 将商品添加到列表中
                 info = {
+                    'ID': id,
                     '品牌': brand,
                     '品名': name,
                     '价格': price,
-                    '评论数': commit,
-                    '好评率': good_commit_percent,
+                    '评论数': comment_count,
+                    '好评率': good_comment_percent,
                     '关键字': tags
                 }
+                # 打印商品信息
+                print(info)
                 data.append(info)
+            except:
+                traceback.print_exc()
+                print(f'error3:在爬取{name}:{driver.current_url}基本信息时出错')
+                good_comments = []
+                mid_comments = []
+                bad_comments = []
+            try:
                 # 搜索评论
+                def get_comment_score(score):
+                    comments = []
+                    comment_page = 0
+                    comment_num = 0
+                    pageSize = 20
+                    while comment_num <= max_comment_num:
+                        comment_json_url = (
+                            "https://api.m.jd.com/?appid=item-v3&functionId=pc_club_productPageComments&"
+                            f"body={{productId:{id},score:{score},sortType:5,page:{comment_page},pageSize:{pageSize}}}")
+                        time.sleep(5)
+                        driver.execute_script(f'''window.open("{comment_json_url}","_blank");''')
+                        comment_page += 1
+                        comment_num += pageSize
+                        # 切换到新标签页
+                        windows = driver.window_handles
+                        driver.switch_to.window(windows[-1])
+                        time.sleep(1)
+                        # 记录数据
+                        comment_json_html = driver.find_elements_by_css_selector('body > pre')[0].text
+                        comment_json = json.loads(comment_json_html)
+                        comments_item = comment_json["comments"]
+                        comments.extend([comment["content"] for comment in comments_item])
+                        # 关闭当前页面
+                        driver.close()
+                        driver.switch_to.window(windows[1])
+                        time.sleep(1)
+                    return comments
+
+                good_comments = get_comment_score(3)
+                print(f'{name}好评爬取完毕，数量{max_comment_num}')
+                mid_comments = get_comment_score(2)
+                print(f'{name}中评爬取完毕，数量{max_comment_num}')
+                bad_comments = get_comment_score(1)
+                print(f'{name}差评爬取完毕，数量{max_comment_num}')
+            except:
+                traceback.print_exc()
+                print(f'error4:在爬取{name}评论:{driver.current_url}出错')
+            finally:
+                with open(f"{comment_dirname}/{id}.json", "w", encoding="utf-8_sig") as f:
+                    phone_comment = {
+                        'good_comments': good_comments,
+                        'mid_comments': mid_comments,
+                        'bad_comments': bad_comments
+                    }
+                    f.write(json.dumps(phone_comment, ensure_ascii=False, indent=4))
                 # 关闭当前页面，返回搜索页
                 driver.close()
                 driver.switch_to.window(windows[0])
-                # 打印商品信息
-                print(info)
-            except:
-                print(f'在爬取{name}:{driver.current_url}时出错')
             product_num += 1
             if product_num >= max_product_num:
                 break
@@ -151,24 +223,21 @@ def get_data(key="5g手机", max_pages=1, max_product_num=10):
         else:
             break
         page_number += 1
-    # 关闭浏览器驱动程序
-    driver.quit()
     return data
 
 
 def save_data(data):
     # 将数据保存到CSV文件中
-    filename = "phone_info_jd.csv"
-    fields = ["品牌", "品名", "价格", "评论数", "好评率", "关键字"]
+    fields = ["ID", "品牌", "品名", "价格", "评论数", "好评率", "关键字"]
 
-    with open(filename, 'w', newline='', encoding='utf_8_sig') as csvfile:
+    with open(phone_info_jd_filepath, 'w', newline='', encoding='utf_8_sig') as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=fields)
         writer.writeheader()
         writer.writerows(data)
 
-    print("数据已保存到", filename)
+    print("数据已保存到", phone_info_jd_filepath)
 
 
 if __name__ == "__main__":
-    data = get_data(KeyWord, MaxPages, MaxProductName)
+    data = get_data(KeyWord, MaxPages, MaxProductName, psort, MaxCommentNum)
     save_data(data)
